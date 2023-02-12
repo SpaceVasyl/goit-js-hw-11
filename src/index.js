88916977be52069572841fc3eb4854c19773b8ff
SpaceVasyl <109691084+SpaceVasyl@users.noreply.github.com>
2023-02-12 20:54:46 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,12 +3,13 @@ import axios from "axios";
 import {findImages} from './findImages';
 const form = document.querySelector('#search-form');
 const submit = document.querySelector('#submit');
+const photoGallery = document.querySelector('.gallery');
 const DEBOUNCE_DELAY = 300;
 
-submit.addEventListener("input", searchPhoto);
+form.addEventListener("submit", searchPhoto);
 function searchPhoto(e){
     e.preventDefault();
-    const inputPhoto = form.value;
+    const inputPhoto = e.target[0].value;
     if(!inputPhoto){
         return;
     }
@@ -17,4 +18,28 @@ function searchPhoto(e){
 
 function createMarkup(c){
     console.log(c);
+    photoGallery.innerHTML = c.map((a)=>{
+    const {webformatURL, largeImageURL, tags, likes, views, comments, downloads} = a;
+    return `
+    <div class="photo-card">
+  <img src="" alt="" loading="lazy" />
+  <div class="info">
+    <p class="info-item">
+      <b>Likes</b>
+    </p>
+    <p class="info-item">
+      <b>Views</b>
+    </p>
+    <p class="info-item">
+      <b>Comments</b>
+    </p>
+    <p class="info-item">
+      <b>Downloads</b>
+    </p>
+  </div>
+</div>
+    `
+})
+
+    console.log(c.largeImageURL);
 }
