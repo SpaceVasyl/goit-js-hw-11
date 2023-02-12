64f575f0cb706983e832bf0a4b8bad9fdf192407
SpaceVasyl <109691084+SpaceVasyl@users.noreply.github.com>
2023-02-12 22:22:25 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -20,29 +20,30 @@ function createMarkup(c){
     console.log(c);
     photoGallery.innerHTML = c.map((a)=>{
     const {webformatURL, largeImageURL, tags, likes, views, comments, downloads} = a;
-    return `
-    <div class="photo-card">
-  <img src="${webformatURL}" alt="${tags}" loading="lazy" />
-  <div class="info">
-    <p class="info-item">
-      <b>Likes</b>
-      ${likes}
-    </p>
-    <p class="info-item">
-      <b>Views</b>
-      ${views}
-    </p>
-    <p class="info-item">
-      <b>Comments</b>
-      ${comments}
-    </p>
-    <p class="info-item">
-      <b>Downloads</b>
-      ${downloads}
-    </p>
-  </div>
-</div>
-    `
+    
+//     return `
+//     <div class="photo-card">
+//   <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+//   <div class="info">
+//     <p class="info-item">
+//       <b>Likes</b>
+//       ${likes}
+//     </p>
+//     <p class="info-item">
+//       <b>Views</b>
+//       ${views}
+//     </p>
+//     <p class="info-item">
+//       <b>Comments</b>
+//       ${comments}
+//     </p>
+//     <p class="info-item">
+//       <b>Downloads</b>
+//       ${downloads}
+//     </p>
+//   </div>
+// </div>
+//     `
 })
 
     console.log(c.largeImageURL);
