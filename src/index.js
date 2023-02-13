6d810a457a413abf9f3a15687427de1dc647369e
SpaceVasyl <109691084+SpaceVasyl@users.noreply.github.com>
2023-02-13 12:34:36 +0200

    s

--- src/index.js
+++ src/index.js
@@ -22,28 +22,24 @@ function createMarkup(r){
     const {webformatURL, largeImageURL, tags, likes, views, comments, downloads} = a;
     console.log(webformatURL);
     return `
-//     <div class="photo-card">
-//   <img src="${webformatURL}" alt="${tags}" loading="lazy" />
-//   <div class="info">
-//     <p class="info-item">
-//       <b>Likes</b>
-//       ${likes}
-//     </p>
-//     <p class="info-item">
-//       <b>Views</b>
-//       ${views}
-//     </p>
-//     <p class="info-item">
-//       <b>Comments</b>
-//       ${comments}
-//     </p>
-//     <p class="info-item">
-//       <b>Downloads</b>
-//       ${downloads}
-//     </p>
-//   </div>
-// </div>
-//     `
-})
+    <div class="photo-card">
+   <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+<div class="info">
+<p class="info-item">
+<b>Likes</b>
+${likes}</p>
+<p class="info-item">
+ <b>Views</b>
+ ${views}</p>
+ <p class="info-item">
+ <b>Comments</b>
+ ${comments}</p>
+ <p class="info-item">
+<b>Downloads</b>
+${downloads}</p>
+</div>
+ </div>
+     `
+}).join('')
 }
 //     
