3e8fc00d980b880881f4a1a37d76de9dc4fc5030
SpaceVasyl <109691084+SpaceVasyl@users.noreply.github.com>
2023-02-13 22:36:53 +0200

    a

--- src/index.js
+++ src/index.js
@@ -23,7 +23,7 @@ function createMarkup(r){
     console.log(webformatURL);
     return `
     <div class="photo-card">
-   <img src="${webformatURL}" alt="${tags}" loading="lazy" width='300'/>
+   <img src="${webformatURL}" alt="${tags}" loading="lazy" width='280'/>
 <div class="info">
 <p class="info-item">
 <b>Likes</b>
